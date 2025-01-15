21b29e5d4e8ce456a2bdfe364af74a1e1ef24878
Fahim Ahmed <personal.fahim00@gmail.com>
2025-01-16 02:27:31 +0600

    added exec timeout, cached schema support

--- core/SQLiteDatabase.py
+++ core/SQLiteDatabase.py
@@ -1,20 +1,22 @@
+import json
 import sqlite3
 from pathlib import Path
 from typing import Optional
+from func_timeout import func_timeout, FunctionTimedOut
 
 class SQLiteDatabase:
     """ Class for dealing with sqlite3 databases. Provides SQL execution capabilities and access to schema"""
-    def __init__(self, db_id: str, input_path: Path, use_cached_schema: Optional[Path] = None) -> None:
+    def __init__(self, db_id: str, input_path: Path, exec_timeout: float = 30.0, use_cached_schema: Optional[Path] = None) -> None:
         """ Attributes
             ----------
                 db_id: str
                     name of database; database must exist in input_path/db_id/db_id.sqlite
                 input_path: Path
-                    parent directory of database folder e.g. input_path/db_id/db_id.sqlite 
-                cursor: sqlite3.Cursor
-                    connected cursor to the database, used to execute all queries
+                    parent directory of database folder
+                exec_timeout: float
+                    maximum number of seconds for query to return a result; aborts returning [(err),]
                 schema: dict[str, str]
-                    ...
+                    either raw_schema or read from json in path 
 
                 raw_schema: dict[str, str]
                     unaugmented, plain db schemas indexed by table_name, read from db_id.sqlite
@@ -23,55 +25,81 @@ class SQLiteDatabase:
                     which exist in input_path/db_id/database_description/
 
                 use_cached_schemas: Path | None
-                    use pre-generated schema stored in input_path/db_id/db_id_augschema.json
-                    instead of raw_schema
+                    use pre-generated schema stored in path/to/aug.json provided
+                    instead of raw_schema. File contains dict of db_id: schema.
         """
         self.db_id = db_id
         self.input_path = input_path
+        self.exec_timeout = exec_timeout
 
-        self.cursor: sqlite3.Cursor = self.__get_db_cursor()
         self.raw_schema: dict[str, str] = self.__fetch_raw_schema()
         self.descriptions: dict[str, str] = self.__fetch_db_descriptions()
 
         if use_cached_schema:
-            self.schema = ... # read from json
+            with open(use_cached_schema, 'r') as f:
+                self.schema = json.load(f)[db_id]
+                print(type(self.schema))
         else:
             self.schema = self.raw_schema
 
     def __getitem__(self, table_name: str):
-        return self.schemas[table_name]
+        """ Return the schema of a table in the database. """
+        return self.schema[table_name] 
     
     def __str__(self):
-        return "\n".join( list(self.schema.values()) )
+        """ Returns the database schema as a human-readable/executable string. """
+        return "\n\n".join( list(self.schema.values()) )
     
-    def run_query(self, sql: str) -> list:
-        """ Executes SQL query and returns rows. """
-        rows = self.cursor.execute(sql).fetchall()
-        return rows
+    def run_query(self, sql: str) -> list[tuple]:
+        """ Executes SQL query and fetches all rows. """
+        try:
+            def execute_sql():
+                db_path = (self.input_path / self.db_id / self.db_id).with_suffix('.sqlite')
+                with sqlite3.connect(db_path, uri=True) as conn:
+                    rows = conn.execute(sql).fetchall()
+                return rows
 
-    def __get_db_cursor(self) -> sqlite3.Cursor:
-        """ Connects to db and returns a read-only cursor. """
-        db_path = (self.input_path / self.db_id / self.db_id).with_suffix('.sqlite')
-        connection = sqlite3.connect(db_path, uri=True)
-        cursor = connection.cursor()
-        return cursor
+            rows = func_timeout(timeout=self.exec_timeout, func=execute_sql)
+        except FunctionTimedOut as timeout_error:
+            rows = [("Error: timedout", )]
+        return rows
     
     def __fetch_raw_schema(self) -> dict[str, str]:
         """ Returns a dict of schema of all tables in a .sqlite database indexed by table name """
-        tables = self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall()
+        tables = self.run_query("SELECT name FROM sqlite_master WHERE type='table';")
         schemas: dict[str, str] = {}
         for table, in tables:
             if table != "sqlite_sequence":
-                schema = self.cursor.execute("SELECT sql FROM sqlite_master WHERE name=?;", [table]).fetchone()[0]
+                schema, = self.run_query(f"SELECT sql FROM sqlite_master WHERE name='{table}';")[0]
                 schemas[table] = schema
         return schemas
     
     def __fetch_db_descriptions(self) -> dict[str, str]:
+        """ Returns a dict of database_descriptions from each table_name.csv as strings  """
+        def case_insensitive_file_reader(filepath: Path):
+            content = f'Descriptions file for table at {filepath} does not exist.'
+            if filepath.exists():
+                with open(filepath, 'r', errors='ignore') as file:
+                    content = file.read()
+            else:
+                file_stem = filepath.stem
+                candidate_stems = [
+                    stem for stem in (
+                        file_stem.capitalize(), file_stem.title(), file_stem.upper(), file_stem.lower()
+                    )
+                    if filepath.with_stem(stem).exists()
+                ]
+                if candidate_stems:
+                    file_stem = candidate_stems[0]
+                    filepath = filepath.with_stem(file_stem)
+                    content = case_insensitive_file_reader(filepath)
+            return content
+        
         descriptions = {}
         for table in self.raw_schema.keys():
-            filename = (self.input_path / self.db_id / 'database_description' / table).with_suffix('.csv')
-            with open(filename, 'r', errors='ignore') as file:
-                descriptions[table] = file.read()
+            filepath = (self.input_path / self.db_id / 'database_description' / table).with_suffix('.csv')
+            descriptions[table] = case_insensitive_file_reader(filepath)
+
         return descriptions
     
 
@@ -81,45 +109,33 @@ if __name__ == '__main__':
     # db_id = 'thrombosis_prediction'
     input_path = Path('data/bird-minidev/dev_databases')
     db = SQLiteDatabase(db_id, input_path)
-    
-    for name, description in db.descriptions.items():
-        print('-' * 25)
-        print(name.capitalize())
-        print('-' * 25)
-        print(description)
-        print('.' * 25)
-        print('.' * 25)
-        print('\n')
-    print(db)
+    rows = db.run_query(
+        "SELECT T2.driverRef "
+        "FROM qualifying AS T1 INNER JOIN drivers AS T2 "
+        "ON T2.driverId = T1.driverId "
+        "WHERE T1.raceId = 20 "
+        "ORDER BY T1.q1 DESC "
+        "LIMIT 5"
+    )
+    print(rows)
 
+    # for name, description in db.descriptions.items():
+    #     print('-' * 25)
+    #     print(name.capitalize())
+    #     print('-' * 25)
+    #     print(description)
+    #     print('.' * 25)
+    #     print('.' * 25)
+    #     print('\n')
+    # print(db)
 
-
-# ### Database Utility Functions ###
-# def get_db_cursor(db_id: str) -> sqlite3.Cursor:
-#     """ Connects to db and returns a cursor. """
-#     db_path = (INPUT_PATH / 'dev_databases/' / db_id / db_id).with_suffix('.sqlite')
-#     connection = sqlite3.connect(db_path)
-#     cursor = connection.cursor()
-#     return cursor
-
-
-# def fetch_BIRD_schemas(db_names: list[str]) -> dict[str, str]:
-#     """ Returns a dictionary of BIRD db_schemas indexed by db_names. """
-    
-#     def fetch_schema(db_id: str) -> str:
-#         """Returns the schema of all tables in a .sqlite database. """
-#         cursor = get_db_cursor(db_id)
-#         cursor = cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-#         tables = cursor.fetchall()
-        
-#         schemas: list[str] = []
-#         for table in tables:
-#             table_name, = table
-#             if table_name != "sqlite_sequence":
-#                 cursor = cursor.execute("SELECT sql FROM sqlite_master WHERE name=?;", [table_name])
-#                 schema = cursor.fetchone()[0]
-#                 schemas.append(schema)
-
-#         return "\n".join(schemas)
-
-#     return {db_id: fetch_schema(db_id) for db_id in db_names}
+    ## Run in jupyter
+    # db_names: list[str] = [f.name for f in input_path.iterdir()]
+    # databases: dict[str, SQLiteDatabase] = {
+    #     db_id: SQLiteDatabase(db_id, input_path) 
+    #     for db_id in db_names
+    # }
+    # for db_id, db in databases.items():
+    #     print(db_id)
+    #     display(db.raw_schema)
+    #     display(db.descriptions)
