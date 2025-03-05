2101ebf4e74ebb3d96bbd382f19b8fb37403b83f
Fahim Ahmed <personal.fahim00@gmail.com>
2025-03-06 02:03:19 +0600

    added debate, reasoner_zs exp

--- experiments.py
+++ experiments.py
@@ -3,34 +3,65 @@ import pandas as pd
 from vllm import LLM, SamplingParams
 from core.birdeval import evaluate
 from core.dbhandler import SQLiteDatabase
-from sqlgen.base_agent import TextToSQL
-from sqlgen.discussion import MultiAgentDiscussion
 from sqlgen.zeroshot import ZeroShotAgent
-from typing import Callable
-
-
-def discuss_experiment(
-    df: pd.DataFrame, databases: dict[str, SQLiteDatabase], llm: LLM, 
-    output_path: Path, batch_size: int, savename: str = f'multiag'
-):
-    MultiAgentDiscussion.discuss(
-        df=df, 
-        databases=databases, 
-        llm=llm, 
-        output_path=output_path, 
-        savename=savename, 
-        batch_size=batch_size, 
-        evaluator_fn=evaluate, 
-    )
+from sqlgen.reason import ReasonerZeroShot
+from sqlgen.discussion import MultiAgentDiscussion
+from sqlgen.debate import MultiAgentDebate
 
 
 def zeroshot_experiment(
     df: pd.DataFrame, databases: dict[str, SQLiteDatabase], 
     llm: LLM, cfg: SamplingParams, 
-    output_path: Path, batch_size: int, savename: str = f'zs'
+    output_path: Path, batch_size: int, savename: str = 'zs'
 ):  
     agent_zs = ZeroShotAgent(llm, databases, output_path)
     outputs, labels = agent_zs.batched_generate(
         df, cfg, batch_size, 
         savename, evaluate
+    )
+
+
+def reasoner_zeroshot_experiment(
+    df: pd.DataFrame, databases: dict[str, SQLiteDatabase], 
+    llm: LLM, cfg: SamplingParams, 
+    output_path: Path, batch_size: int, savename: str = 'rzs'
+):  
+    agent_rzs = ReasonerZeroShot(llm, databases, output_path)
+    outputs, labels = agent_rzs.batched_generate(
+        df, cfg, batch_size, 
+        savename, evaluate
+    )
+
+
+def discuss_experiment(
+    df: pd.DataFrame, databases: dict[str, SQLiteDatabase], 
+    llm: LLM, cfg: SamplingParams,
+    output_path: Path, batch_size: int, savename: str = 'mad'
+):
+    MultiAgentDiscussion.discuss(
+        df=df, 
+        databases=databases, 
+        llm=llm,
+        cfg=cfg,
+        output_path=output_path, 
+        batch_size=batch_size,
+        savename=savename, 
+        evaluator_fn=evaluate, 
+    )
+
+
+def debate_experiment(
+    df: pd.DataFrame, databases: dict[str, SQLiteDatabase], 
+    llm: LLM, cfg: SamplingParams,
+    output_path: Path, batch_size: int, savename: str = 'madb'
+):
+    MultiAgentDebate.debate(
+        df=df, 
+        databases=databases, 
+        llm=llm,
+        cfg=cfg,
+        output_path=output_path, 
+        batch_size=batch_size,
+        savename=savename, 
+        evaluator_fn=evaluate, 
     )
