20d115095360fb355d2f1d5c7601c2dc3d7ae163
Fahim Ahmed <personal.fahim00@gmail.com>
2025-03-12 20:11:12 +0600

    enabled prefix cache, increased max tokens

--- main.py
+++ main.py
@@ -21,19 +21,19 @@ def setup_experiment():
         args.MODEL.value,
         gpu_memory_utilization=args.GPU_MEMORY_UTILIZATION,
         tensor_parallel_size=args.TENSOR_PARALLEL_SIZE,
-        # max_model_len=args.MODEL_MAX_SEQ_LEN,
-        # max_seq_len_to_capture=args.MODEL_MAX_SEQ_LEN,
+        max_model_len=args.MODEL_MAX_SEQ_LEN,
         kv_cache_dtype=args.KV_CACHE_DTYPE,
         seed=args.SEED,
         dtype=args.VLLM_DTYPE,
         trust_remote_code=True,
         enforce_eager=True,
+        enable_prefix_caching=True,
     )
     cfg = SamplingParams(
         temperature=0,
         top_p=1,
         repetition_penalty=1.1,
-        max_tokens=4096,
+        max_tokens=4096*2,
     )
     # Create Output Directory
     args.OUTPUT_PATH.mkdir(parents=True, exist_ok=True)
