51b0bbb0e649298b577af9af166eba2b2d96e4ce
Fahim Ahmed <personal.fahim00@gmail.com>
2025-03-19 20:46:39 +0600

    added picker experiment

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from experiments import (
     reasoner_zeroshot_experiment,
     discuss_experiment,
     debate_experiment,
+    reasoner_picker_experiment,
 )
 
 
@@ -75,6 +76,21 @@ if __name__ == '__main__':
                 max_tokens=1024*2,
             )
             debate_experiment(df, databases, llm, cfg, args.OUTPUT_PATH, args.BATCH_SIZE, args.EXPERIMENT)
+        # case 'pick':
+        #     rzn_cfg = SamplingParams(
+        #         temperature=0.6,
+        #         top_p=0.95,
+        #         top_k=30,
+        #         repetition_penalty=1.0,
+        #         max_tokens=4096*2,
+        #     )
+        #     code_cfg = SamplingParams(
+        #         temperature=0,
+        #         top_p=1,
+        #         repetition_penalty=1.05,
+        #         max_tokens=1024,
+        #     )
+        #     reasoner_picker_experiment(df, databases, rzn_llm, code_llm, rzn_cfg, code_cfg, args.OUTPUT_PATH, args.BATCH_SIZE, args.EXPERIMENT)
         case _:
             print("INVALID EXPERIMENT SELECTED. ABORTING.")
     
