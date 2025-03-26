31c2ad262bade233b5444785318bf4045c7e2974
Fahim Ahmed <personal.fahim00@gmail.com>
2025-03-26 15:38:59 +0600

    added exp timer, made llm load per experiment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
 from vllm import LLM, SamplingParams
-from utils import read_dataset, parse_args
+from utils import read_dataset, parse_args, load_llm, del_llm
 from experiments import (
     zeroshot_experiment,
     reasoner_zeroshot_experiment,
@@ -9,41 +10,23 @@ from experiments import (
 )
 
 
-def setup_experiment():
+if __name__ == '__main__':
     # Parse Arguments
     args = parse_args()
+    # Create Output Directory
+    args.OUTPUT_PATH.mkdir(parents=True, exist_ok=True)
     # Read Dataset
     df, databases = read_dataset(
         args.INPUT_PATH, args.BIRD_QUESTION_FILENAME, args.DB_FOLDERNAME, 
         args.USE_CACHED_SCHEMA, args.DB_EXEC_TIMEOUT, args.IS_DEBUG,
     )
-    # LLM and Default Generation Config
-    llm = LLM(
-        args.MODEL.value,
-        gpu_memory_utilization=args.GPU_MEMORY_UTILIZATION,
-        tensor_parallel_size=args.TENSOR_PARALLEL_SIZE,
-        max_model_len=args.MODEL_MAX_SEQ_LEN,
-        kv_cache_dtype=args.KV_CACHE_DTYPE,
-        seed=args.SEED,
-        dtype=args.VLLM_DTYPE,
-        trust_remote_code=True,
-        enforce_eager=True,
-        enable_prefix_caching=True,
-    )
-    # Create Output Directory
-    args.OUTPUT_PATH.mkdir(parents=True, exist_ok=True)
-    return df, databases, llm, args
 
+    start_time = datetime.datetime.now()
+    print(f"Starting Experiment: {args.EXPERIMENT} with {args.MODEL.value} at {start_time}")
 
-
-if __name__ == '__main__':
-    df, databases, llm, args = setup_experiment()
-    experiment = args.EXPERIMENT
-
-    print(f"Starting Experiment: {experiment} with {args.MODEL.value}")
-
-    match experiment:
+    match args.EXPERIMENT:
         case 'zs':
+            llm = load_llm(args)
             cfg = SamplingParams(
                 temperature=0,
                 top_p=1,
@@ -51,7 +34,9 @@ if __name__ == '__main__':
                 max_tokens=1024,
             )
             zeroshot_experiment(df, databases, llm, cfg, args.OUTPUT_PATH, args.BATCH_SIZE, args.EXPERIMENT)
+
         case 'rzs':
+            llm = load_llm(args)
             cfg = SamplingParams(
                 temperature=0.6,
                 top_p=0.95,
@@ -60,7 +45,9 @@ if __name__ == '__main__':
                 max_tokens=4096*2,
             )
             reasoner_zeroshot_experiment(df, databases, llm, cfg, args.OUTPUT_PATH, args.BATCH_SIZE, args.EXPERIMENT)
+
         case 'mad':
+            llm = load_llm(args)
             cfg = SamplingParams(
                 temperature=0,
                 top_p=1,
@@ -68,7 +55,9 @@ if __name__ == '__main__':
                 max_tokens=1024*2,
             )
             discuss_experiment(df, databases, llm, cfg, args.OUTPUT_PATH, args.BATCH_SIZE, args.EXPERIMENT)
+
         case 'madb':
+            llm = load_llm(args)
             cfg = SamplingParams(
                 temperature=0,
                 top_p=1,
@@ -76,6 +65,7 @@ if __name__ == '__main__':
                 max_tokens=1024*2,
             )
             debate_experiment(df, databases, llm, cfg, args.OUTPUT_PATH, args.BATCH_SIZE, args.EXPERIMENT)
+
         # case 'pick':
         #     rzn_cfg = SamplingParams(
         #         temperature=0.6,
@@ -91,7 +81,8 @@ if __name__ == '__main__':
         #         max_tokens=1024,
         #     )
         #     reasoner_picker_experiment(df, databases, rzn_llm, code_llm, rzn_cfg, code_cfg, args.OUTPUT_PATH, args.BATCH_SIZE, args.EXPERIMENT)
+        
         case _:
             print("INVALID EXPERIMENT SELECTED. ABORTING.")
-    
-    print(f"End of Experiment: {experiment} with {args.MODEL.value}")
+
+    print(f"End of Experiment: {args.EXPERIMENT} with {args.MODEL.value}.\nTime taken: {datetime.datetime.now() - start_time}")
