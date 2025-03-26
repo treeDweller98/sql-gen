6baf9e33e1f6693d4ae054c5087f126f14a976ac
Fahim Ahmed <personal.fahim00@gmail.com>
2025-03-26 15:59:33 +0600

    moved llm and cfg boilerplate to experiment.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import datetime
-from vllm import LLM, SamplingParams
-from utils import read_dataset, parse_args, load_llm, del_llm
+from utils import read_dataset, parse_args
 from experiments import (
     zeroshot_experiment,
     reasoner_zeroshot_experiment,
@@ -26,62 +25,15 @@ if __name__ == '__main__':
 
     match args.EXPERIMENT:
         case 'zs':
-            llm = load_llm(args)
-            cfg = SamplingParams(
-                temperature=0,
-                top_p=1,
-                repetition_penalty=1.05,
-                max_tokens=1024,
-            )
-            zeroshot_experiment(df, databases, llm, cfg, args.OUTPUT_PATH, args.BATCH_SIZE, args.EXPERIMENT)
-
+            zeroshot_experiment(args, df, databases)
         case 'rzs':
-            llm = load_llm(args)
-            cfg = SamplingParams(
-                temperature=0.6,
-                top_p=0.95,
-                top_k=30,
-                repetition_penalty=1.0,
-                max_tokens=4096*2,
-            )
-            reasoner_zeroshot_experiment(df, databases, llm, cfg, args.OUTPUT_PATH, args.BATCH_SIZE, args.EXPERIMENT)
-
+            reasoner_zeroshot_experiment(args, df, databases)
         case 'mad':
-            llm = load_llm(args)
-            cfg = SamplingParams(
-                temperature=0,
-                top_p=1,
-                repetition_penalty=1.05,
-                max_tokens=1024*2,
-            )
-            discuss_experiment(df, databases, llm, cfg, args.OUTPUT_PATH, args.BATCH_SIZE, args.EXPERIMENT)
-
+            discuss_experiment(args, df, databases)
         case 'madb':
-            llm = load_llm(args)
-            cfg = SamplingParams(
-                temperature=0,
-                top_p=1,
-                repetition_penalty=1.05,
-                max_tokens=1024*2,
-            )
-            debate_experiment(df, databases, llm, cfg, args.OUTPUT_PATH, args.BATCH_SIZE, args.EXPERIMENT)
-
-        # case 'pick':
-        #     rzn_cfg = SamplingParams(
-        #         temperature=0.6,
-        #         top_p=0.95,
-        #         top_k=30,
-        #         repetition_penalty=1.0,
-        #         max_tokens=4096*2,
-        #     )
-        #     code_cfg = SamplingParams(
-        #         temperature=0,
-        #         top_p=1,
-        #         repetition_penalty=1.05,
-        #         max_tokens=1024,
-        #     )
-        #     reasoner_picker_experiment(df, databases, rzn_llm, code_llm, rzn_cfg, code_cfg, args.OUTPUT_PATH, args.BATCH_SIZE, args.EXPERIMENT)
-        
+            debate_experiment(args, df, databases)
+        case 'pick':
+            reasoner_picker_experiment(args, df, databases)
         case _:
             print("INVALID EXPERIMENT SELECTED. ABORTING.")
 
