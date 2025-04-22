8c032c5f77ad535277ff37091ccbfe5a41b493f3
Fahim Ahmed <personal.fahim00@gmail.com>
2025-04-22 09:21:38 +0600

    removed perriod

--- sqlgen/zeroshot.py
+++ sqlgen/zeroshot.py
@@ -1,8 +1,6 @@
 import pandas as pd
 from core.base_agent import TextToSQL
 
-# TODO: long prompts performs poorly with small models
-
 
 class ZeroShotAgent(TextToSQL):
     """ Zero-shot SQL Generator based on OpenAI Cookbook's "Natural Language to SQL" example and zero-shot COT. """            
@@ -10,7 +8,7 @@ class ZeroShotAgent(TextToSQL):
         prompt = (
             "Given the following SQLite tables, your job is to write a query to answer the given question.\n\n"
             f"### SCHEMA\n{schema}\n\n"
-            f"### QUESTION\n{question}.\n\n"
+            f"### QUESTION\n{question}\n\n"
             f"### YOUR RESPONSE\nLet's think step by step"
         )
         return prompt
@@ -22,7 +20,7 @@ class ReasonerZeroShot(TextToSQL):
         prompt = (
             "Given the following SQLite tables, your job is to write a query to answer the given question.\n\n"
             f"### SCHEMA\n{schema}\n\n"
-            f"### QUESTION\n{question}.\n\n"
+            f"### QUESTION\n{question}\n\n"
             f"### YOUR RESPONSE"
         )
         return prompt
