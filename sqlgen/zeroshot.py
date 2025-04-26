7b25b87871e2b8d8b1447e8840f41e0f9156d148
Fahim Ahmed <personal.fahim00@gmail.com>
2025-04-26 17:55:53 +0600

    new zs prompt

--- sqlgen/zeroshot.py
+++ sqlgen/zeroshot.py
@@ -2,15 +2,39 @@ import pandas as pd
 from core.base_agent import TextToSQL
 
 
+# class ZeroShotAgent(TextToSQL):
+#     """ Zero-shot SQL Generator based on OpenAI Cookbook's "Natural Language to SQL" example and zero-shot COT. """            
+#     def generate_prompt(self, schema: str, question: str) -> str:
+#         prompt = (
+#             "Given the following SQLite tables, your job is to write a query to answer the given question.\n\n"
+#             f"### SCHEMA\n{schema}\n\n"
+#             f"### QUESTION\n{question}\n\n"
+#             f"### YOUR RESPONSE\nLet's think step by step"
+#         )
+#         return prompt
+    
+
+# class ReasonerZeroShot(TextToSQL):
+#     """ Zero-shot SQL generator utilizing a reasoning llm"""            
+#     def generate_prompt(self, schema: str, question: str) -> str:
+#         prompt = (
+#             "Given the following SQLite tables, your job is to write a query to answer the given question.\n\n"
+#             f"### SCHEMA\n{schema}\n\n"
+#             f"### QUESTION\n{question}\n\n"
+#             f"### YOUR RESPONSE"
+#         )
+#         return prompt
+
+
 class ZeroShotAgent(TextToSQL):
     """ Zero-shot SQL Generator based on OpenAI Cookbook's "Natural Language to SQL" example and zero-shot COT. """            
     def generate_prompt(self, schema: str, question: str) -> str:
         prompt = (
-            "Given the following SQLite tables, your job is to write a query to answer the given question.\n\n"
-            f"### SCHEMA\n{schema}\n\n"
-            f"### QUESTION\n{question}\n\n"
-            f"### YOUR RESPONSE\nLet's think step by step"
-        )
+            "Given the following tables, your job is to write a SQLite query to answer the question.\n\n"
+            f"{schema}\n\n"
+            f"{question}\n\n"
+            f"Let's think step by step"
+        ).replace("\n\n", " ")
         return prompt
     
 
@@ -18,9 +42,8 @@ class ReasonerZeroShot(TextToSQL):
     """ Zero-shot SQL generator utilizing a reasoning llm"""            
     def generate_prompt(self, schema: str, question: str) -> str:
         prompt = (
-            "Given the following SQLite tables, your job is to write a query to answer the given question.\n\n"
-            f"### SCHEMA\n{schema}\n\n"
-            f"### QUESTION\n{question}\n\n"
-            f"### YOUR RESPONSE"
-        )
+            "Given the following tables, your job is to write a SQLite query to answer the question.\n\n"
+            f"{schema}\n\n"
+            f"{question}"
+        ).replace("\n\n", " ")
         return prompt
